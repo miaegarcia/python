d53c0b3f51dace5a8de75ccb542a80ce49ff662d
miaegarcia <miagarcia@berkeley.edu>
2024-10-17 15:59:51 -0700

    Add files via upload

---
+++ i'm_sorree.py
@@ -0,0 +1,4 @@
+print(((1*7*24*60*60*1000000000)+(6*24*60*60*1000000000)+(23*60*5*1000000000)+(41*1000000000))**12)
+
+# this number represents the number of nanoseconds late i'm finishing this assigment, to the power of the original due date 
+# it's very late so i'm sorry
